35f1f56de381544f6f4a7345670e7ecc1697a2db
denwaotoko <eurioh316@gmail.com>
2014-01-04 17:33:13 -0800

    Update Transaction.java

--- core/src/main/java/com/google/dogecoin/core/Transaction.java
+++ core/src/main/java/com/google/dogecoin/core/Transaction.java
@@ -617,7 +617,7 @@ public class Transaction extends ChildMessage implements Serializable {
                 s.append(scriptPubKey);
                 s.append(" ");
                 s.append(bitcoinValueToFriendlyString(out.getValue()));
-                s.append(" DOGE");
+                s.append(" TIPS");
                 if (!out.isAvailableForSpending()) {
                     s.append(" Spent");
                 }

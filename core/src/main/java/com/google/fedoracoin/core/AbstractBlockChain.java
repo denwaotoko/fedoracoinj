108d406b45ad7b55501ff1e343cf0799fa26a438
denwaotoko <eurioh316@gmail.com>
2014-01-07 08:10:07 -0800

    Update AbstractBlockChain.java
    A NEW HOPE

--- core/src/main/java/com/google/fedoracoin/core/AbstractBlockChain.java
+++ core/src/main/java/com/google/fedoracoin/core/AbstractBlockChain.java
@@ -841,6 +841,7 @@ public abstract class AbstractBlockChain {
         // Limit the adjustment step.
         final int targetTimespan = params.getTargetTimespan();
         // Limit the adjustment step.
+        /*
         if (storedPrev.getHeight()+1 > 10000)
         {
             if (timespan < targetTimespan / 4)
@@ -861,7 +862,27 @@ public abstract class AbstractBlockChain {
                 timespan = targetTimespan / 16;
             if (timespan > targetTimespan * 4)
                 timespan = targetTimespan * 4;
-        }
+        }*/
+        if (storedPrev.getHeight()+1 >nHeight > 312000)
+        { //New Protocol
+                //nTargetTimespan = 10 * 60; // Retarget every 10 blocks (10 minutes)
+                //nTargetSpacing = 1 * 60; // 60 seconds
+                //nInterval = nTargetTimespan / nTargetSpacing;
+                nActualTimespanMax = nTargetTimespan * (112/100); //12% down
+                nActualTimespanMin = nTargetTimespan * (100/111); //10% up
+                if (timespan < targetTimespan * (112/100))
+                timespan = targetTimespan * (112/100);
+                if (timespan > targetTimespan * (100/111))
+                timespan = targetTimespan * (100/111);
+        }/*
+        else
+        { //Old Protocol
+                nTargetTimespan = 60 * 1.5 * 40; // Retarget every 90 blocks (1 hour)
+                nTargetSpacing = 1 * 40; // 40 seconds
+                nInterval = nTargetTimespan / nTargetSpacing;
+                nActualTimespanMax = nTargetTimespan*4;
+                nActualTimespanMin = nTargetTimespan/4;
+        }*/
 
         BigInteger newDifficulty = Utils.decodeCompactBits(prev.getDifficultyTarget());
         newDifficulty = newDifficulty.multiply(BigInteger.valueOf(timespan));
